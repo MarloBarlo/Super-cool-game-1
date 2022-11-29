97736f2ee89ec87079331e2b6db6ed07b719d020
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-11-28 21:37:21 -0600

    Imported Player.cpp and Player.h Added player to the grid

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "raylib.h"
 #include "rlgl.h"
 #include "raymath.h"
-
+#include "fls/objects/Player.h"
 int main() {
 
     // Variables
@@ -9,7 +9,8 @@ int main() {
     const int screenWidth = 800;
     const int screenHeight = 450;
     Camera2D camera = { 0 };
-    camera.zoom = 0.5f;
+    camera.zoom = .75f;
+    Player newbie = Player(500, 250);
 
     //----------------------------------------------------------------------------------
 
@@ -35,13 +36,13 @@ int main() {
         //----------------------------------------------------------------------------------
         BeginDrawing();
             ClearBackground(WHITE);
-
             BeginMode2D(camera);
                 rlPushMatrix();
                     rlTranslatef(0, 25*50, 0);
                     rlRotatef(90, 1, 0, 0);
                     DrawGrid(100, 50);
                 rlPopMatrix();
+                newbie.drawPlayer();
             EndMode2D();
 
 
