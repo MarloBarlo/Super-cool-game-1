4b0004ac0e15a4cdd9af1e2a6cc8cd728a5c5b94
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-11-28 21:20:46 -0600

    Added a grid so we do calculations.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include "raylib.h"
-
+#include "rlgl.h"
+#include "raymath.h"
 
 int main() {
+
+    // Variables
+    //----------------------------------------------------------------------------------
     const int screenWidth = 800;
     const int screenHeight = 450;
+    Camera2D camera = { 0 };
+    camera.zoom = 0.5f;
+
+    //----------------------------------------------------------------------------------
+
+    // Setup
+    //----------------------------------------------------------------------------------
 
     InitWindow(screenWidth, screenHeight, "super-cool-game-1");
 
     SetTargetFPS(60);
+
+    //----------------------------------------------------------------------------------
 
     while (!WindowShouldClose())
     {
@@ -21,7 +34,15 @@ int main() {
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
+            ClearBackground(BLACK);
 
+            BeginMode2D(camera);
+                rlPushMatrix();
+                    rlTranslatef(0, 25*50, 0);
+                    rlRotatef(90, 1, 0, 0);
+                    DrawGrid(100, 50);
+                rlPopMatrix();
+            EndMode2D();
 
 
         EndDrawing();
