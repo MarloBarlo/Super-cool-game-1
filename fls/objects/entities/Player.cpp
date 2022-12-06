b3464567359e76d608c2b663f1459b64d97d6aa8
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-12-06 13:03:12 -0600

    Cleaned up Player.cpp

--- fls/objects/entities/Player.cpp
+++ fls/objects/entities/Player.cpp
@@ -12,44 +12,14 @@
 
 void Player::drawUpdate(Vector2 mouseLocation) {
 
-    Vector2 followPoint = {location.x, location.y};
-    Vector2 topLeft = {location.x - 25, location.y - 25};
-    Vector2 topRight = {location.x + 25, location.y - 25};
+
     Rectangle playerRect = {location.x, location.y, 50, 50};
     Vector2 triangle = {mouseLocation.x - location.x, (mouseLocation.y - location.y) * -1};
-    //std::cout << mouseLocation.x << " " << mouseLocation.y << std::endl;
     rotation = atan(triangle.x / triangle.y) * 180/M_PI;
-    std::cout << rotation << std::endl;
-
-    Vector2 topLeftPrime = {float(location.x * cos(rotation) - (-1 * location.y * sin(rotation))),
-                            float(location.x * sin(rotation) + (-1 * location.y * cos(rotation)))};
-
-
-
-
     DrawRectanglePro(playerRect, {25, 25}, rotation, BLUE);
-    //std::cout << mouseLocation.x << " " << mouseLocation.y << std::endl;
-    //std::cout << location.x << " " << location.y << std::endl;
+
 }
 
-void Player::draw() {
-    Vector2 topLeft = {location.x - 25, location.y - 25};
-    Vector2 topRight = {location.x + 25, location.y - 25};
-    Vector2 followPoint = {location.x, location.y};
-
-
-
-
-    Rectangle playerRect = {location.x, location.y, 50, 50};
-    DrawRectanglePro(playerRect, {25, 25}, rotation, BLUE);
-    DrawRectanglePro({location.x - 25, location.y - 25, 50, 8}, {0, 0}, rotation, BLACK);
-    //DrawLineEx(topLeft, topRight, 8, BLACK);
-    std::cout << location.x << " " << location.y << std::endl;
-
-
-
-
-}
 
 Player::Player(float x, float y) {
     location.x = x;
