cd7bcb6c5e437f2d36672bdfb2e2137658e044df
Jordan Deuley <114099341+dewy413@users.noreply.github.com>
2022-12-06 13:02:18 -0600

    Cleaned up main.

--- main.cpp
+++ main.cpp
@@ -14,10 +14,7 @@ int main() {
     const int screenHeight = 450;
     InitWindow(screenWidth, screenHeight, "super-cool-game-1");
     Player newbie = Player(0, 0);
-    Camera2D camera = {};
-    camera.zoom = .50f;
-    camera.rotation = 0;
-    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    Camera2D camera = {Vector2{ screenWidth/2.0f, screenHeight/2.0f }, 0, 0, 0, .50};
     Vector2 mouseLocation = GetScreenToWorld2D(GetMousePosition(), camera);
 
 
@@ -27,7 +24,6 @@ int main() {
     //----------------------------------------------------------------------------------
 
     InitWindow(screenWidth, screenHeight, "super-cool-game-1");
-
     SetTargetFPS(60);
 
     //----------------------------------------------------------------------------------
@@ -36,14 +32,16 @@ int main() {
     {
         // Update
         //----------------------------------------------------------------------------------
-        camera.target = Vector2{newbie.location.x, newbie.location.y};
+        camera.target = newbie.location;
         mouseLocation = GetScreenToWorld2D(GetMousePosition(), camera);
         float wheel = GetMouseWheelMove();
+
         if (wheel != 0) {
             const float zoomIncrement = 0.125f;
             camera.zoom += (wheel*zoomIncrement);
             if (camera.zoom < zoomIncrement) camera.zoom = zoomIncrement;
         }
+
         if (IsKeyDown(KEY_D)) newbie.location.x += newbie.speed;
         if (IsKeyDown(KEY_A)) newbie.location.x -= newbie.speed;
         if (IsKeyDown(KEY_W)) newbie.location.y -= newbie.speed;
@@ -51,7 +49,6 @@ int main() {
         if (IsKeyDown(KEY_LEFT_SHIFT)) newbie.speed = 4.0, newbie.stamina -= .10;
         else newbie.speed = 2.0;
         if (newbie.stamina <= 0) newbie.speed = 2.0;
-        //newbie.rotation += 1;
 
         //----------------------------------------------------------------------------------
 
